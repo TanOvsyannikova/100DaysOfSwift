0d5fd40b500db9d641b8e2b6e6bd3858cf7b835f
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-02-19 13:17:03 +0300

    day 9 commit. Structs. Self

---
+++ Day9/self.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+var person = Person(name: "tan")
